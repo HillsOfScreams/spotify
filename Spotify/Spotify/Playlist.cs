65dfacd5f9961e081c9ca392bee30d1a28132028
HillsOfScreams <tomhubers100@gmail.com>
2023-04-13 14:17:44 +0200

    Version 1 met playlist en song beetje gedaan

---
+++ Spotify/Spotify/Playlist.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Spotify
+{
+    public class Song
+    {
+        public string name;
+        public string artistName;
+        public float duration;
+
+        public Song(string name, string artistName, float duration)
+        {
+            this.name = name;
+            this.artistName = artistName;
+            this.duration = duration;
+        }
+    }
+    public class Playlist
+    {
+        public string? PlaylistName;
+        private List<Song> songs;
+        public void addSongToList(Song song)
+        {
+            songs.Add(song);
+        }
+
+        public Playlist(string koek)
+        {
+            songs = new List<Song>();
+            this.PlaylistName = koek;
+        }
+
+        public List<Song> returnSong()
+        {
+            return songs;
+        }
+
+        public static void PrintSong(Playlist playlist)
+        {
+            var songs = playlist.returnSong();
+            Console.WriteLine("Name of playlist: " + playlist.PlaylistName);
+            Console.WriteLine(" ");
+            Console.WriteLine("Song(s) in this playlist: ");
+
+            for (int i = 0; i < songs.Count; i++)
+            {
+                Console.WriteLine(songs[i].name);
+            }
+        }
+    }
+}
---
+++ Spotify/Spotify/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using static Spotify.Program;
+using Spotify;
+
+namespace Spotify
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what would you like to do?");
+            Console.WriteLine("make playlist" + " TEST" + "addsong");
+            string action = Console.ReadLine();
+            if (action == "make playlist")
+            {
+                Console.WriteLine("dis nog ni af hahaha");
+            }
+            else if(action == "TEST")
+            {
+                Playlist testList = new Playlist("kaduwa");
+                Song macarena = new Song("macarena", "testartist", 114);
+                testList.addSongToList(macarena);
+                Playlist.PrintSong(testList);
+            }
+
+
+
+
+
+
+        }
+        
+    }
+}
